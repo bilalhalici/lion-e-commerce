3e6a0993d1f11bf3f0de6aa49da49ec601293acf
Bilal <bila.halici@gmail.com>
2023-11-25 00:57:20 +0300

    feed: created authentication component alongside sign in form

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,18 +1,18 @@
 import './button.styles.scss';
 
-const BUTTON_TYPES_CLASSES = {
-  'google': 'google-sign-in',
-  'inverted': 'inverted',
+const BUTTON_TYPE_CLASSES = {
+  google: 'google-sign-in',
+  inverted: 'inverted',
 };
 
-const Button = ({ children, buttonType, otherProps }) => {
+const Button = ({ children, buttonType, ...otherProps }) => {
   return (
-    <div
-      className={`button-container ${BUTTON_TYPES_CLASSES[buttonType]}`}
+    <button
+      className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
       {...otherProps}
     >
       {children}
-    </div>
+    </button>
   );
 };
 
