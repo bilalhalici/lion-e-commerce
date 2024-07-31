ef609270dd70a45643245a53813d2dd433efacec
kemal.caglar <kemal.caglar@hellosmpl.com>
2024-07-31 15:01:33 +0300

    new entegrasyon notification

--- public/smpl-notification-sw.js
+++ public/smpl-notification-sw.js
@@ -1,2 +1,1 @@
-// eslint-disable-next-line no-undef
-importScripts("https://d370pv1i0ks4ah.cloudfront.net/smpl-script.js");
+importScripts("https://d370pv1i0ks4ah.cloudfront.net/smpl-notification-sw.js");
